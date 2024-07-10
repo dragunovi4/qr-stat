968ecab00788fbd54efb4ea07b056cff4a62e3cf
romanchesko <dumhammerr@gmail.com>
2024-07-10 13:22:37 +0500

    Отправка по времени и отправка в группу зафиналенная рабочая

--- src/main/java/ru/quickresto/qrstatsbot/QrStatStApplication.java
+++ src/main/java/ru/quickresto/qrstatsbot/QrStatStApplication.java
@@ -2,14 +2,14 @@ package ru.quickresto.qrstatsbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class QrStatStApplication {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(QrStatStApplication.class, args);
-		SendMessageAtSpecifiedTime sendMessageAtSpecifiedTime = new SendMessageAtSpecifiedTime();
-		sendMessageAtSpecifiedTime.sendMessageAtSpecifiedTime();
 	}
 }
